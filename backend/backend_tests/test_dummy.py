45a3815bd835f1dbf0e56eb09fecf391fcebb5b6
shakedHUJI <142309542+shakedHUJI@users.noreply.github.com>
2024-10-16 15:52:24 +0300

    added ci/cd versions and backend test

--- backend/backend_tests/test_dummy.py
+++ backend/backend_tests/test_dummy.py
@@ -2,4 +2,4 @@ def test_dummy():
     assert True, "This test should always pass"
 
 def test_dummy_2():
-    assert 1 == 1, "This test should also always pass"
+    assert 1 != 1, "This test should also always pass"
